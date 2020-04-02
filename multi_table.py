8949b07381a420e4989052570b9f409e03b13a89
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-02 14:04:24 -0400

    solved issues

--- multi_table.py
+++ multi_table.py
@@ -1,13 +1,15 @@
 #create multiplication table
-
+count = 1
 stop = 1
 power = 1
-while power <= 10:
-    while stop <= 10:
-        num = (power * stop)
-        print(f'{power} X {stop} = {num}')
-        stop += 1
-    power += 1
-
+while count <= 100:
+    while power <= 10:
+        while stop <= 10:
+            num = (power * stop)
+            print(f'{power} X {stop} = {num}')
+            stop += 1
+        power += 1
+        stop -= 9
+    count += 1
 
 
