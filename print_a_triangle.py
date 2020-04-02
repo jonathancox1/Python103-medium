d89a1f356b0ac3d38aa2a6dfc4af15e67585844a
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-02 14:18:00 -0400

    solution

--- print_a_triangle.py
+++ print_a_triangle.py
@@ -1,29 +1,16 @@
-#a = int(input('How wide would you like the base of the triangle to be?  '))
-
-a = 4
-b = (a - 1) / 2
+#draw a silly little triangle with a base width of 5 '*'
+a = 5
+b = (a + 1) / 2
 ast = 1
-while a > 0:
+while a > 1:
     spaces = ' '
     trict = '*'
     while b > 0:
         print((int(b) * spaces) + (ast * trict) + (int(b) * spaces))
         a -= 1
         b -= 1
-        ast += 1
+        ast += 2
     print(ast * trict)
     a -= 1
-    ast += 1
+    ast += 2
     
-
-
-
-
-
-
-# while a > 0:
-#     while a > b:
-#         spaces = ' '
-#         print(spaces * int(b) + '*')
-#         b -= 1
-#     a -= 1
