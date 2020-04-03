1e0de606acb9de7695eed29fa03111d9811486bb
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-03 13:25:14 -0400

    added comments

--- print_a_box.py
+++ print_a_box.py
@@ -4,13 +4,12 @@ height = int(input('Height?  '))
 #prints top border
 print('*' * width)
 
-#creates new value for squares that are larger than 2x2
-adjusted_height = height - 2
+
 
 #loops through the print sequence to add spaces between outside boarder while height is more than 2
 while height > 2:
     spaces = ' '
-    print('*' + ((width -2) * spaces) + '*')
+    print('*' + ((width -2) * spaces) + '*') #addes spaces into the center of box not border (width -2)
     height -= 1
     
     
